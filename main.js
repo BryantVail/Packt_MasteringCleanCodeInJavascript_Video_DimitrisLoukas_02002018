5e6c06f2d19e488e226d0e9b74834e9dfb281a8b
Bryant Vail <Bryanttv@outlook.com>
2018-03-09 15:57:17 -0500

    fatherName vs fName

--- main.js
+++ main.js
@@ -1,1 +1,79 @@
 
+/*
+  
+  meaningful variables:
+*/
+    //tip#1: Favor more descriptive names rather than short names.
+    let person  = {
+      fName = "John",
+      lastName  = "Vail",
+      fatherName = "Lewis"
+    }
+    
+    console.log(`father's name is: ${fName}`);//this could be done on accident
+//arrival
+var time1 = new Date();
+
+//departure
+var time2 = new Date();
+//if you need comments to describe your code, 
+//names should be MEMORABLE, go between files quickly
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
