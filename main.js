621deb462351a0d72828aaa1dbb6ba2dc4f386e4
bxvail <BryantVail@gmail.com>
2018-03-11 23:38:13 -0400

    linter init

--- main.js
+++ main.js
@@ -5,42 +5,14 @@
     avoid 'buggy' code regarding 'this'
 
 */
-// tip#15:
-//    Use Object.freeze instead of 'const'
+//
+//    Using a linter
 //
 
 ///////////////////////////////////
 /////// Objects & Classes /////////
-///////  Object.freeze    /////////
+///////  linter           /////////
 ///////////////////////////////////
-
-//const ensures that the variable declared is pointing to the same object, 
-//it does not govern the maintenance of the values within the object
-/*
-  config = {};//error bc I have tried to change the object associated w/config
-*/
-
-const config = {
-  fbApiKey: 'snfa9f7yh43f30j03t4hg34',
-  googleApiKey:'4jhu954htgh39g343g',
-  linkedInApiKey:'jo2045349jnfinl3436787'
-};
-Object.freeze(config);//cannot change values of the 'config' object's properties, if the apiKey is changed, then nothing will happen
-
-console.log(config);
-
-config.googleApiKey = "";//TypeError: Cannot assign to read only property 'googleApiKey' of object '#<Object>'
-console.log(config);
-
-/*
-{ fbApiKey: 'snfa9f7yh43f30j03t4hg34',
-  googleApiKey: '4jhu954htgh39g343g',
-  linkedInApiKey: 'jo2045349jnfinl3436787' }
-
-{ fbApiKey: 'snfa9f7yh43f30j03t4hg34',
-  googleApiKey: '',
-  linkedInApiKey: 'jo2045349jnfinl3436787' }
-*/
 
 
 
