9162c47e13cef4c3fec76caeebcb22ee8b58123b
bxvail <BryantVail@gmail.com>
2018-03-12 00:23:28 -0400

    npm install eslint --save-dev

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 "use strict";
-//CleanCodeWJavaScript_CleanFunctions_AllAboutThis_ObjectsAndClasses_Object.freeze
+//CleanCodeWJavaScript_CleanFunctions_AllAboutThis_ObjectsAndClasses_linter-init
 /*
     The content of 'this'
     avoid 'buggy' code regarding 'this'
@@ -14,7 +14,20 @@
 ///////  linter           /////////
 ///////////////////////////////////
 
-
+/*
+  -what is a linter
+    static code analysis, 
+    avoid bugs & problems
+    saves debugging time
+    forces better written code
+  -Pros & Cons
+  -Most Famous JavaScript linters
+    JSLint
+    JSCS
+    JSHint
+    JSLint
+  -adding 'ESLint' to your project
+*/
 
 
 
