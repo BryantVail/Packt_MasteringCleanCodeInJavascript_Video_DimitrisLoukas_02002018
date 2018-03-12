23e2230c974468eb9c0d84f11920108ca1a5dbee
bxvail <BryantVail@gmail.com>
2018-03-11 22:54:25 -0400

    callMeLater(test.bind(obj));
    callMeLater(test.bind(obj));//no matter the context of the calling function, test is 'bound' to the obj 'this' context
    // .bind() DOES NOT call the function
    // returns a ref to the function w/the 'this' keyword set to the function specified

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//CleanCodeWJavaScript_CleanFunctions_AllAboutThis_init
+//CleanCodeWJavaScript_CleanFunctions_AllAboutThis_HardBinding
 /*
     The content of 'this'
     avoid 'buggy' code regarding 'this'
@@ -7,32 +7,85 @@
 // 1.Function definition location DOES NOT matter
 //    -its the call site
 
+///////////////////////////////////
+////////Hard Binding //////////////
+///////////////////////////////////
+
+var obj = {
+  a:1,
+  b:2,
+  c:3
+}
+
+function test(){
+  console.log(this.a);
+  console.log(this.b);
+  console.log(this.c);
+}
+
+function callMeLater(cb){
+  setTimeout(()=> {
+    cb();
+  }, 3000);
+}//end function callMeLater(cb){
+
+callMeLater(test.bind(obj));//no matter the context of the calling function, test is 'bound' to the obj 'this' context
+// .bind() DOES NOT call the function
+// returns a ref to the function w/the 'this' keyword set to the function specified
 
 
-  var a = 92;
 
-  var obj = {
-    a:1,
-    b:2,
-    c:3,
-  };
 
-  function test(){
-    console.log(this.a);
-  }
-  test();//global call
 
-  obj.test = test;//a reference to the 'function test(){'
-  obj.test();//call from the context of the object
-  //console results:
-  //  92
-  //  1
 
-  test.call(obj); //additional parameters are sent in as an array[]
-  // obj.test = test; &&  obj.test(); at once
-  //function test, call with the 'this' context of 'obj'
 
-  test.apply(obj, );//additional parameters ar passed in as a set of arguments
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
