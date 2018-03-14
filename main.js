008fdde03ace661c0dd5bf672e7f6c59a8b4ef0b
Bryant Vail <Bryanttv@outlook.com>
2018-03-14 14:36:14 -0400

    Anti-Pattern

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
 let todos = [
-  {task:  "Buy Eggs",                          completed:false},
+  {task:  "Buy Eggs",                         completed:false},
   {task:  "Fight for rights",                 completed:false},
   {task:  "Stand up for what you believe in", completed:true},
   {task:  "Make a difference",                completed:true}
   ]//end todos[]
+
+function makePending(todos){
+  for(let i=0;i<todos.length;i++){
+    todos.completed = false;
+}//end makePending(todos
+
+
+
+console.log(todos);
+console.log();
+makePending(todos);
+console.log(todos);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
