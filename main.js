21ca44111d003103a89d507e890a96b306d339b8
Bryant Vail <Bryanttv@outlook.com>
2018-03-29 17:23:23 -0400

    reduce function

--- main.js
+++ main.js
@@ -4,17 +4,32 @@ let numbers = [
   2,9,8,7,6
  ];//end numbers
 
-function numbersSum(numbers){
-  let sum = 0;
-  for(let num of numbers){
-    if(num % 2 ==0){
-      sum +=num/2;
+
+function numberSum(numbers){
+  return numbers.reduce((accumulator, current) => {
+    if(isEven(current)){
+      return accumulator +current /2; 
     }
     else{
-      sum +=num;
+      return accumulator + current;
     }//end else
-  }//end for(let num of numbers){
-  return sum;//should be 24
+  },0);//end numbers.reduce
+}
+//function numbersSum(numbers){
+//  let sum = 0;
+//  for(let num of numbers){
+//    if(num % 2 ==0){
+//      sum +=num/2;
+//    }
+//    else{
+//      sum +=num;
+//   }//end else
+//  }//end for(let num of numbers){
+//  return sum;//should be 24
+  
+  function isEven(num){
+     return (num % 2 ==0)//return true/false 
+  }
   
 }//end numbersSum
 
