b0233b455efab789227405edded459c5dfcf25ae
Bryant Vail <Bryanttv@outlook.com>
2018-04-03 13:19:13 -0400

    module

--- module-pattern.js
+++ module-pattern.js
@@ -1,1 +1,46 @@
 //module-pattern.js
+
+function ModuleLoader(){
+  var modules = {};
+  
+  function getInstance(name){
+    if(modules[name]){
+        return modules[name];
+    }
+    else{
+        throw new Error(`Undefined module: ${name}`);
+    }//end if else
+  }//end getInstance
+  
+  
+  function define(name, dependencies, instance){
+      if(modules[name]){
+           throw new Error(`Module ${name} already defined`);
+      }
+      else{
+        var depsInstances  = dependencies.map(dep) => {
+            return getInstance(dep);
+        }//end anonymous function from dependencies.map(dep)
+      }//end else
+  }//end define
+    
+}//end ModuleLoader
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
