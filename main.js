b76b58db34d4a25e79dd94a126e0a4f854b5a1a0
Bryant Vail <Bryanttv@outlook.com>
2018-06-02 16:05:04 -0700

    add identifier

--- main.js
+++ main.js
@@ -12,12 +12,35 @@ let todos = [
 
 function makePending(todos){
 
-      for(let i =0;i<todos.length; i++){
-            todos[i].completed = false;
-      }//end for
+    //   for(let i =0;i<todos.length; i++){
+    //         todos[i].completed = false;
+	//   }//end for   CTRL+K, CTRL+C
+	
+	// //3 arguments: 
+	// 	Item(todo item in this case), 
+	// 	index, 
+	// 	can also take a whole array?"dont need it right now"
+	todos.forEach((todo, index)=> {
+		todo.completed = false;
+	});
+
+	//Array.prototype.map
+	//constructs a new array from the values in the function
+
+	function addIdentifier(todos){
+		for(let i =0;i<todos.length; i++){
+			todos[i].id = i+1;
+		}
+	}//end addIdentifier
+
+	console.log(`before adding identifier: \n${todos}`);
+
+	addIdentifier(todos);
+	console.log(`after adding identifier: \n${todos}`);
+
 }//end makePending(todos)
 console.log(todos);
-
+console.log();
 makePending(todos);
 console.log(todos);
 
