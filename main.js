56abc011cd37998c970bbb3937bd5cb5c13357ec
Bryant Vail <Bryanttv@outlook.com>
2018-06-02 15:42:59 -0700

    foreach init

--- main.js
+++ main.js
@@ -1,31 +1,7 @@
-let todos = [
-  {task:  "Buy Eggs",                         completed:false},
-  {task:  "Fight for rights",                 completed:false},
-  {task:  "Stand up for what you believe in", completed:true},
-  {task:  "Make a difference",                completed:true}
-  ]//end todos[]
-
-function makePending(todos){
-  for(let i=0;i<todos.length;i++){
-    todos.completed = false;
-}//end makePending(todos
+"use strict";//always use strict
 
 
-
-console.log(todos);
-console.log();
-makePending(todos);
-console.log(todos);
-
-
-
-
-
-
-
-
-
-
+//FunctionalProgramming_PureFunction_ini
 
 
 
