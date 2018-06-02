166fd5508bb744a0e151dde9ac71d107fbffc9de
Bryant Vail <Bryanttv@outlook.com>
2018-06-02 15:47:19 -0700

    todos entered

--- main.js
+++ main.js
@@ -3,7 +3,14 @@
 
 //foreach & map
 
+let todos = [
+      {task: "buy eggs",      completed:  false},
+      {task: "Feed the dog",  completed:  true}, 
+      {task: "Study for Net+",completed:  false},
+      {task: "Cook Dinner",   completed:  false}
+];
 
+console.log(todos);
 
 
 
