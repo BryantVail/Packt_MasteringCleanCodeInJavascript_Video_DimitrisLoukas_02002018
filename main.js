0d32fc4f17e22c82093516e452248f4c4bf60e07
Bryant Vail <Bryanttv@outlook.com>
2018-06-02 16:08:29 -0700

    init

--- main.js
+++ main.js
@@ -10,26 +10,17 @@ let todos = [
       {task: "Cook Dinner",   completed:  false}
 ];
 
-function makePending(todos){
+	//Array.prototype.map
+	//constructs a new array from the values in the function
 
-    //   for(let i =0;i<todos.length; i++){
-    //         todos[i].completed = false;
-	//   }//end for   CTRL+K, CTRL+C
-	
-	// //3 arguments: 
-	// 	Item(todo item in this case), 
-	// 	index, 
-	// 	can also take a whole array?"dont need it right now"
-	todos.forEach((todo, index)=> {
-		todo.completed = false;
-	});
+	function addIdentifier(todos){
+		for(let i =0;i<todos.length; i++){
+			todos[i].id = i+1;
+		}
+	}//end addIdentifier
 
-}//end makePending(todos)
+	console.log(`before adding identifier: \n${todos}`);
 
-console.log(todos);
-console.log();
-makePending(todos);
-console.log(todos);
-
-
+	addIdentifier(todos);
+	console.log(`after adding identifier: \n${todos}`);
 
