50eed009f012ec594460102e0f295044d4ae9a61
Bryant Vail <Bryanttv@outlook.com>
2018-06-27 16:47:00 -0400

    restart

--- main.js
+++ main.js
@@ -9,30 +9,22 @@
 
 
 
-  var a = 92;
+var a = 92;
 
-  var obj = {
-    a:1,
-    b:2,
-    c:3,
-  };
+var obj = {
+  a:1,
+  b:2,
+  c:3
+};
 
-  function test(){
-    console.log(this.a);
-  }
-  test();//global call
+function test(){
+  console.log(this.a);
+}
 
-  obj.test = test;//a reference to the 'function test(){'
-  obj.test();//call from the context of the object
-  //console results:
-  //  92
-  //  1
+test();
 
-  test.call(obj); //additional parameters are sent in as an array[]
-  // obj.test = test; &&  obj.test(); at once
-  //function test, call with the 'this' context of 'obj'
-
-  test.apply(obj, );//additional parameters ar passed in as a set of arguments
+obj.test = test;
+obj.test();
 
 
 
